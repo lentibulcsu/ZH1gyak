8428baa86376270f933a662397d54381c5735e90
Bulcsú András Lenti <bulcsu.lenti@stud.uni-corvinus.hu>
2022-10-12 10:28:44 +0200

    commitolom ma is

--- ZH1_negyedik_01/ZH1_negyedik_01/Form1.cs
+++ ZH1_negyedik_01/ZH1_negyedik_01/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace ZH1_negyedik_01
 {
     public partial class Form1 : Form
     {
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
